34058b87722bc13d4473b056770ceb031c5b5281
Nick Kuria <nkuria01@gmail.com>
2021-03-31 10:33:45 +0300

    Added books using factory function

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 let myLibrary = [
-{author : "author 1",
-title: "book1",
-pages: 300,
-read: true},
-{author : "author 2",
-title: "book2",
-pages: 300,
-read: false},
+// {author : "author 1",
+// title: "book1",
+// pages: 300,
+// read: true},
+// {author : "author 2",
+// title: "book2",
+// pages: 300,
+// read: false},
 ];
 
 const Book = (author,title,pages,read) => {
 
     return {author,title,pages,read};
 }
+myLibrary.push(Book("author 1", "book 1", 300, true));
+myLibrary.push(Book("author 2", "book 2", 500, true));
 function addBookToLibrary(){}
 
 function bookDisplay(arr){
@@ -38,6 +40,16 @@ function bookDisplay(arr){
         titleText = document.createTextNode(arr[i].title);
         title.appendChild(titleText);
         container.appendChild(title);
+
+        const pages = document.createElement('p');
+        pagesText = document.createTextNode(arr[i].pages);
+        pages.appendChild(pagesText);
+        container.appendChild(pages);
+
+        const read = document.createElement('p');
+        readText = document.createTextNode(arr[i].read);
+        read.appendChild(readText);
+        container.appendChild(read);
 
     }
 
